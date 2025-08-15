c3fb76a9da59e9b0944bee0273e9276a075bb138
ydw <eodnjs7783@yonsei.ac.kr>
2025-08-16 02:09:56 +0900

    Convert remaining submodules to normal folders

---
+++ submodules/toolchain/README.md
@@ -0,0 +1,6 @@
+# toolchainHYVRID
+- Manager : Kweon HyeokJin
+- **arm-buildroot-linux-gnueabi**
+- Several revision from HYVRID
+- Add/Replace some kernel header to sysroot (e.g. gpio.h, version.h)
+- Managed as submodules
---
+++ submodules/toolchain/lib/gcc/arm-buildroot-linux-gnueabi/8.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/opt/armv5-eabi--glibc--stable-2020.02-2 --sysconfdir=/opt/armv5-eabi--glibc--stable-2020.02-2/etc --enable-static --target=arm-buildroot-linux-gnueabi --with-sysroot=/opt/armv5-eabi--glibc--stable-2020.02-2/arm-buildroot-linux-gnueabi/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/opt/armv5-eabi--glibc--stable-2020.02-2 --with-mpc=/opt/armv5-eabi--glibc--stable-2020.02-2 --with-mpfr=/opt/armv5-eabi--glibc--stable-2020.02-2 --with-pkgversion='Buildroot 2020.02.2' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --enable-threads --without-isl --without-cloog --with-float=soft --with-abi=aapcs-linux --with-cpu=arm926ej-s --with-float=soft --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/opt/armv5-eabi--glibc--stable-2020.02-2/arm-buildroot-linux-gnueabi/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "arm926ej-s" }, { "float", "soft" }, { "mode", "arm" }, { "tls", "gnu" } };
